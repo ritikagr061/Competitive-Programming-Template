68bf538977e8997225a67b613a205732fc2ecd74
Ritik Agrawal <54122273+ritikagr061@users.noreply.github.com>
2023-07-02 03:48:51 +0530

    Update Graphs.cpp

--- Graphs.cpp
+++ Graphs.cpp
@@ -2,31 +2,45 @@
  
 using namespace std;
 
-bool isBipartite(int n,vector<int> adj[])
+bool bipartiteHelper(int i, vector<int> adj[], vector<int> &col)
 {
-  vector<int> col(n+1,0);
-  queue<int> q;
-	q.push(1);
-	col[1] = 1; //red
-	//red=1,blue=0
-  
-	while (!q.empty())
-	{
-		int x = q.front();
-		int color = col[x];
-		q.pop();
-		for (auto it : adj[x])
-		{
-			if (!col[it])
-				q.push(it),col[it] = (color == 1 ? 2 : 1);
-			else
-				if (color == col[it]) return false;
-		}
-	}
- 
-	return true;
+    queue<int> q;
+    q.push(i);
+    col[i] = 1; //red
+
+    while (!q.empty())
+    {
+        int popped = q.front();
+        int color = col[popped];
+        q.pop();
+        for (auto it : adj[popped])
+        {
+            if (!col[it])
+            {
+                q.push(it);
+                col[it] = (color == 1 ? 2 : 1);
+            }
+            else if (color == col[it])
+                return false;
+        }
+    }
+
+    return true;
 }
 
+bool isBipartite(int n, vector<int> adj[])
+{
+    vector<int> col(n + 1, 0);
+
+    for (int i = 1; i <= n; i++) {
+        if (col[i] == 0) {
+            if (!bipartiteHelper(i, adj, col))
+                return false;
+        }
+    }
+
+    return true;
+}
 
 bool isCyclic_nonDirect_helper(int currNode, int n, vector<int> adj[], vector<bool> &vis, int parent) {
     vis[currNode] = true;
